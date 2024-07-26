30fc397705fc0b469d44433b6427421bf43632e4
sharan <sharanreddy799@gmail.com>
2024-07-26 04:53:44 +0000

    Added User Authentication using the AuthController class and SecurityConfig.java to create authentication manager bean

---
+++ src/main/java/com/example/lms/controller/AuthController.java
@@ -0,0 +1,62 @@
+package com.example.lms.controller;
+
+import com.example.lms.entity.User;
+import com.example.lms.request.LoginRequest;
+import com.example.lms.request.RegisterRequest;
+import com.example.lms.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+
+
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/login")
+    public String authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return "User authenticated successfully";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody RegisterRequest registerRequest) {
+        if(userService.existsByUsername(registerRequest.getUsername())) {
+            return "Username is already taken";
+        }
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setRole(registerRequest.getRole());
+        if(userService.saveUser(user)!=null){
+            return "User registered successfully";
+        } else {
+            return "User registration Error";
+        }
+    }
+}
+
+
